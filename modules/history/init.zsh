16b59b0a0c71695298d12540581788c855086307
Larry Gouger <larry.gouger@coxautoinc.com>
2019-09-19 10:32:15 -0400

    my mods

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -12,7 +12,8 @@
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt SHARE_HISTORY             # Share history between all sessions.
+# setopt SHARE_HISTORY             # Share history between all sessions.
+setopt INC_APPEND_HISTORY_TIME
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
@@ -25,8 +26,10 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 #
 # Variables
 #
+FULL_TTY=`tty`
+TTY=`basename $FULL_TTY`
 
-HISTFILE="${HISTFILE:-${ZDOTDIR:-$HOME}/.zhistory}"  # The path to the history file.
+HISTFILE="${HISTFILE:-${ZDOTDIR:-$HOME}/.zhistory.d/$TTY}"  # The path to the history file.
 HISTSIZE=10000                   # The maximum number of events to save in the internal history.
 SAVEHIST=10000                   # The maximum number of events to save in the history file.
 
